3e224f804ed784a210ca4eff870baba8eb151861
Sue <sanchezsilvasue@gmail.com>
2026-05-04 20:02:11 +0200

    sorted problems in main.py

--- app/main.py
+++ app/main.py
@@ -16,9 +16,8 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
 my_favourite_films = ["interstellar", "the matrix", "amelie"]
-    sorted_variables = {
+sorted_variables = {
     "mutable": [b, d, my_favourite_films, collection_of_coins],
     "immutable": [a, c, my_name, age]
 }
