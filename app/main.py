feb5b09e09be42c2c580e2554a9606cbdd3941aa
Sue <sanchezsilvasue@gmail.com>
2026-05-04 19:10:29 +0200

    fix error in main.py

--- app/main.py
+++ app/main.py
@@ -17,4 +17,4 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 # write your code here
-my_favourite_films = [interstellar, the matrix, amelie]
+my_favourite_films = ["interstellar", "the matrix", "amelie"]
